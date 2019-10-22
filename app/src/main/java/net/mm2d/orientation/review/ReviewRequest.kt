b310028f88cd772a2ebfb388d6540dc5c24c769f
OHMAE Ryosuke <ryo@mm2d.net>
2019-10-23 00:01:41 +0900

    🐛check lifecycle

--- app/src/main/java/net/mm2d/orientation/review/ReviewRequest.kt
+++ app/src/main/java/net/mm2d/orientation/review/ReviewRequest.kt
@@ -8,6 +8,7 @@
 package net.mm2d.orientation.review
 
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
 import net.mm2d.orientation.control.OrientationHelper
 import net.mm2d.orientation.settings.Settings
 import java.util.concurrent.TimeUnit
@@ -31,6 +32,9 @@ object ReviewRequest {
     }
 
     fun requestReviewIfNeed(activity: FragmentActivity) {
+        if (activity.lifecycle.currentState != Lifecycle.State.RESUMED) {
+            return
+        }
         val settings = Settings.get()
         if (settings.reported || settings.reviewed) {
             return
