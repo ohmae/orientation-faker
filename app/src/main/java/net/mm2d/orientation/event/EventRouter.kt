6cad92bdeb0b40de93b47adcc081604b8f99e135
OHMAE Ryosuke <ryo@mm2d.net>
2019-11-24 14:48:23 +0900

    ♻simplify

--- app/src/main/java/net/mm2d/orientation/event/EventRouter.kt
+++ app/src/main/java/net/mm2d/orientation/event/EventRouter.kt
@@ -7,16 +7,40 @@
 
 package net.mm2d.orientation.event
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 import net.mm2d.orientation.event.Event.EVENT_UPDATE
 
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
 object EventRouter {
+    private val subject: Subject<Event> = PublishSubject.create<Event>()
+
     fun notifyUpdate() {
-        EventBus.notify(EVENT_UPDATE)
+        subject.onNext(EVENT_UPDATE)
     }
 
     fun createUpdateObserver(): EventObserver =
         RxEventObserver(EVENT_UPDATE)
+
+    internal class RxEventObserver(
+        private val event: Event
+    ) : EventObserver {
+        private var disposable: Disposable? = null
+
+        override fun subscribe(callback: () -> Unit) {
+            disposable?.dispose()
+            disposable = subject
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter { it == event }
+                .subscribe { callback.invoke() }
+        }
+
+        override fun unsubscribe() {
+            disposable?.dispose()
+        }
+    }
 }
