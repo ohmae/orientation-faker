09c1848cd2e22a3e4e94f98198cdf9a59b01c1dc
OHMAE Ryosuke <ryo@mm2d.net>
2020-04-08 22:06:08 +0900

    ✨ForegroundPackageSettings

---
+++ app/src/main/java/net/mm2d/orientation/control/ForegroundPackageSettings.kt
@@ -0,0 +1,55 @@
+package net.mm2d.orientation.control
+
+import android.content.Context
+import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.mm2d.orientation.room.database.PackageSettingsDatabase
+import net.mm2d.orientation.room.entity.PackageSettingEntity
+
+object ForegroundPackageSettings {
+    private const val DB_NAME = "package_settings.db"
+    private lateinit var database: PackageSettingsDatabase
+    private val map: MutableMap<String, Int> = mutableMapOf()
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+
+    fun initialize(context: Context) {
+        database = Room.databaseBuilder(context, PackageSettingsDatabase::class.java, DB_NAME).build()
+        scope.launch {
+            val all = database.packageSettingsDao().getAll()
+            withContext(Dispatchers.Main) {
+                all.forEach {
+                    map[it.packageName] = it.orientation
+                }
+            }
+        }
+    }
+
+    fun updateInstalledPackages(packages: List<String>) {
+        val list = map.keys.toMutableList()
+        list.removeAll(packages)
+        list.forEach { map.remove(it) }
+        scope.launch {
+            list.forEach { database.packageSettingsDao().delete(it) }
+        }
+    }
+
+    fun get(packageName: String): Int = map.getOrElse(packageName) { Orientation.INVALID }
+
+    fun put(packageName: String, orientation: Int) {
+        if (orientation == Orientation.INVALID) {
+            map.remove(packageName)
+            scope.launch {
+                database.packageSettingsDao().delete(packageName)
+            }
+        } else {
+            map[packageName] = orientation
+            scope.launch {
+                val entity = PackageSettingEntity(packageName, orientation)
+                database.packageSettingsDao().insert(entity)
+            }
+        }
+    }
+}
