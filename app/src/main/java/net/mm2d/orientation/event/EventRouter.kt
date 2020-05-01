d9d92b1338132e06324c58eb81a249b8eae9cea5
OHMAE Ryosuke <ryo@mm2d.net>
2020-05-01 17:08:12 +0900

    ♻use coroutines channel

--- app/src/main/java/net/mm2d/orientation/event/EventRouter.kt
+++ app/src/main/java/net/mm2d/orientation/event/EventRouter.kt
@@ -7,40 +7,51 @@
 
 package net.mm2d.orientation.event
 
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.subjects.PublishSubject
-import io.reactivex.subjects.Subject
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import net.mm2d.orientation.event.Event.EVENT_UPDATE
 
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
+@FlowPreview
+@ExperimentalCoroutinesApi
 object EventRouter {
-    private val subject: Subject<Event> = PublishSubject.create()
+    private val channel: BroadcastChannel<Event> = BroadcastChannel(1)
 
     fun notifyUpdate() {
-        subject.onNext(EVENT_UPDATE)
+        GlobalScope.launch {
+            channel.send(EVENT_UPDATE)
+        }
     }
 
     fun createUpdateObserver(): EventObserver =
-        RxEventObserver(EVENT_UPDATE)
+        ChannelEventObserver(EVENT_UPDATE)
 
-    internal class RxEventObserver(
+    internal class ChannelEventObserver(
         private val event: Event
     ) : EventObserver {
-        private var disposable: Disposable? = null
+        private var job: Job? = null
 
         override fun subscribe(callback: () -> Unit) {
-            disposable?.dispose()
-            disposable = subject
-                .observeOn(AndroidSchedulers.mainThread())
-                .filter { it == event }
-                .subscribe { callback.invoke() }
+            job?.cancel()
+            job = GlobalScope.launch {
+                channel.asFlow()
+                    .filter { it == event }
+                    .collect {
+                        withContext(Dispatchers.Main) {
+                            callback.invoke()
+                        }
+                    }
+            }
         }
 
         override fun unsubscribe() {
-            disposable?.dispose()
+            job?.cancel()
+            job = null
         }
     }
 }
