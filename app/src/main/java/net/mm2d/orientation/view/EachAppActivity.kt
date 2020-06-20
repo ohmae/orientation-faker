e0e133159318be1b0219805783ad59858e7a7357
OHMAE Ryosuke <ryo@mm2d.net>
2020-06-21 00:52:54 +0900

    ✨add search apps

--- app/src/main/java/net/mm2d/orientation/view/EachAppActivity.kt
+++ app/src/main/java/net/mm2d/orientation/view/EachAppActivity.kt
@@ -7,12 +7,15 @@ import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.view.isGone
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_each_app.*
@@ -27,20 +30,21 @@ import net.mm2d.orientation.settings.Settings
 import net.mm2d.orientation.util.SystemSettings
 import net.mm2d.orientation.view.dialog.EachAppOrientationDialog
 import net.mm2d.orientation.view.dialog.UsageAppPermissionDialog
+import java.util.*
 
 class EachAppActivity : AppCompatActivity(), EachAppOrientationDialog.Callback {
     private val settings by lazy {
         Settings.get()
     }
-    private lateinit var adapter: Adapter
+    private var adapter: Adapter? = null
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_each_app)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        package_check.setOnClickListener { togglePackageCheck() }
         recycler_view.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        setUpSearch()
         scope.launch {
             val list = makeAppList()
             withContext(Dispatchers.Main) {
@@ -49,13 +53,36 @@ class EachAppActivity : AppCompatActivity(), EachAppOrientationDialog.Callback {
         }
     }
 
+    private fun setUpSearch() {
+        search_window.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) = Unit
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter?.search(s.toString())
+            }
+        })
+        search_window.setOnEditorActionListener { _, _, _ ->
+            hideKeyboard()
+            true
+        }
+    }
+
     private fun setAdapter(list: List<AppInfo>) {
         ForegroundPackageSettings.updateInstalledPackages(list.map { it.packageName })
-        adapter = Adapter(this, list) { position, packageName ->
+        Adapter(this, list) { position, packageName ->
+            hideKeyboard()
             EachAppOrientationDialog.show(this, position, packageName)
+        }.let {
+            adapter = it
+            recycler_view.adapter = it
+            it.search(search_window.text.toString())
         }
-        recycler_view.adapter = adapter
         progress_bar.visibility = View.GONE
+    }
+
+    private fun hideKeyboard() {
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+            .hideSoftInputFromWindow(search_window.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 
     override fun onDestroy() {
@@ -65,7 +92,7 @@ class EachAppActivity : AppCompatActivity(), EachAppOrientationDialog.Callback {
 
     override fun onResume() {
         super.onResume()
-        applyPackageCheck()
+        package_check_disabled.isGone = settings.foregroundPackageCheckEnabled
     }
 
     override fun onPostResume() {
@@ -73,6 +100,20 @@ class EachAppActivity : AppCompatActivity(), EachAppOrientationDialog.Callback {
         if (!SystemSettings.hasUsageAccessPermission(this)) {
             UsageAppPermissionDialog.showDialog(this)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.each_app, menu)
+        (menu.findItem(R.id.package_check).actionView as SwitchCompat).also {
+            it.isChecked = settings.foregroundPackageCheckEnabled
+            it.setOnCheckedChangeListener { _, isChecked ->
+                hideKeyboard()
+                settings.foregroundPackageCheckEnabled = isChecked
+                package_check_disabled.isGone = isChecked
+                MainService.update(this)
+            }
+        }
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -84,20 +125,10 @@ class EachAppActivity : AppCompatActivity(), EachAppOrientationDialog.Callback {
     }
 
     override fun onChangeSettings(position: Int) {
-        adapter.notifyItemChanged(position)
+        adapter?.notifyItemChanged(position)
         if (OrientationHelper.isEnabled) {
             MainService.start(this)
         }
-    }
-
-    private fun togglePackageCheck() {
-        settings.foregroundPackageCheckEnabled = !settings.foregroundPackageCheckEnabled
-        applyPackageCheck()
-        MainService.update(this)
-    }
-
-    private fun applyPackageCheck() {
-        package_check.isChecked = settings.foregroundPackageCheckEnabled
     }
 
     private fun makeAppList(): List<AppInfo> {
@@ -129,14 +160,43 @@ class EachAppActivity : AppCompatActivity(), EachAppOrientationDialog.Callback {
         var icon: Drawable? = null
     }
 
+    private class DiffCallback(
+        private val old: List<AppInfo>,
+        private val new: List<AppInfo>
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = old.size
+        override fun getNewListSize(): Int = new.size
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            old[oldItemPosition] == new[newItemPosition]
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            old[oldItemPosition] == new[newItemPosition]
+    }
+
     class Adapter(
         private val context: Context,
-        private val list: List<AppInfo>,
+        private val initialList: List<AppInfo>,
         private val listener: (position: Int, packageName: String) -> Unit
     ) : RecyclerView.Adapter<ViewHolder>() {
         private val inflater: LayoutInflater = LayoutInflater.from(context)
         private val scope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+        private var list: List<AppInfo> = initialList
         private var defaultIcon: Drawable? = null
+        private var searchWord: String = ""
+
+        fun search(word: String) {
+            val w = word.toLowerCase(Locale.ENGLISH)
+            if (w == searchWord) return
+            searchWord = w
+            val newList = initialList.filter { it.contains(w) }
+            val diff = DiffUtil.calculateDiff(DiffCallback(list, newList))
+            list = newList
+            diff.dispatchUpdatesTo(this)
+        }
+
+        private fun AppInfo.contains(word: String): Boolean =
+            label.toLowerCase(Locale.ENGLISH).contains(word) ||
+                packageName.toLowerCase(Locale.ENGLISH).contains(word)
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
             ViewHolder(inflater.inflate(R.layout.li_each_app, parent, false))
