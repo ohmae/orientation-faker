fc91ab3aa80a3d5cbe4b963740564bafb82523e6
OHMAE Ryosuke <ryo@mm2d.net>
2020-08-26 22:15:57 +0900

    🎉release 4.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 
 val applicationName = "OrientationFaker"
 val versionMajor = 4
-val versionMinor = 2
-val versionPatch = 10
+val versionMinor = 3
+val versionPatch = 0
 
 android {
     compileSdkVersion(29)
