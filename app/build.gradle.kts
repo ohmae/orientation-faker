25247048d242e378586945de1c11de6753d9b594
OHMAE Ryosuke <ryo@mm2d.net>
2020-10-08 08:13:22 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,29 +62,29 @@ android {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.browser:browser:1.2.0")
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
     implementation("androidx.lifecycle:lifecycle-process:2.2.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.preference:preference:1.1.1")
     implementation("com.google.android.material:material:1.2.1")
-    implementation("com.google.android.play:core:1.8.0")
+    implementation("com.google.android.play:core:1.8.2")
     implementation("com.google.android.play:core-ktx:1.8.1")
     implementation("androidx.room:room-runtime:2.2.5")
     implementation("androidx.room:room-ktx:2.2.5")
     kapt("androidx.room:room-compiler:2.2.5")
-    implementation("net.mm2d:log:0.9.2")
-    implementation("net.mm2d:log-android:0.9.2")
+    implementation("net.mm2d:log:0.9.3")
+    implementation("net.mm2d:log-android:0.9.3")
     implementation("net.mm2d:color-chooser:0.2.0")
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.4")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.5")
 }
 
 fun isStable(version: String): Boolean {
