6b294ce58cfb50cd88237476dc2653d699d94ba8
OHMAE Ryosuke <ryo@mm2d.net>
2021-01-14 00:08:51 +0900

    📖add header

--- app/src/main/java/net/mm2d/orientation/util/ColorExtension.kt
+++ app/src/main/java/net/mm2d/orientation/util/ColorExtension.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.orientation.util
 
 import android.graphics.Color
--- app/src/main/java/net/mm2d/orientation/util/NoStateLiveData.kt
+++ app/src/main/java/net/mm2d/orientation/util/NoStateLiveData.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021 大前良介 (OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.orientation.util
 
 import androidx.lifecycle.MutableLiveData
@@ -5,6 +12,8 @@ import androidx.lifecycle.MutableLiveData
 class NoStateLiveData<T> : MutableLiveData<T>() {
     override fun setValue(value: T) {
         super.setValue(value)
-        super.setValue(null)
+        if (value != null) {
+            super.setValue(null)
+        }
     }
 }
