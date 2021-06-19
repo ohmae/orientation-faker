b7b6dff0a1ea89f7137a7bf6175b1b221b5a8345
OHMAE Ryosuke <ryo@mm2d.net>
2021-06-19 12:48:31 +0900

    🆙flipper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,10 +88,10 @@ dependencies {
     implementation("net.mm2d.color-chooser:color-chooser:0.2.5")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
-    debugImplementation("com.facebook.flipper:flipper:0.91.1")
+    debugImplementation("com.facebook.flipper:flipper:0.94.1")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.91.1")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.91.1")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.94.1")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.94.1")
 }
 
 fun isStable(version: String): Boolean {
