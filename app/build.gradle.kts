2f9d05b067746e1609c0e4b08a21fdf144ad62bc
OHMAE Ryosuke <ryo@mm2d.net>
2021-10-30 15:19:33 +0900

    🆙androidx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,13 +66,13 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.browser:browser:1.3.0")
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-process:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-process:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.preference:preference-ktx:1.1.1")
@@ -88,10 +88,10 @@ dependencies {
     implementation("net.mm2d.color-chooser:color-chooser:0.3.0")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
-    debugImplementation("com.facebook.flipper:flipper:0.115.0")
+    debugImplementation("com.facebook.flipper:flipper:0.117.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.115.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.115.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.117.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.117.0")
 }
 
 fun isStable(version: String): Boolean {
