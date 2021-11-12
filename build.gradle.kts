bcf2ba11fc1bba07fada8e9c2d189653ccb85632
OHMAE Ryosuke <ryo@mm2d.net>
2021-11-12 09:50:10 +0900

    🆙gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,6 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.create("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "orientation-faker"
 include(":app")
