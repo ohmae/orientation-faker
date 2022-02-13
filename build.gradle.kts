51908b320b8f192de245b48183ba6f71dd948efc
OHMAE Ryosuke <ryo@mm2d.net>
2022-02-13 14:47:50 +0900

    🆙jetpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.1")
         classpath(kotlin("gradle-plugin", version = "1.6.10"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.41.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
     }
 }
 
