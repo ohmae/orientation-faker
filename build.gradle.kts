6f088788caff5ce5d35192d3d74ab1661c2abb0c
OHMAE Ryosuke <ryo@mm2d.net>
2022-05-12 21:29:55 +0900

    ♻use plugin management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,11 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0")
-        classpath(kotlin("gradle-plugin", version = "1.6.21"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
-    }
+plugins {
+    id("com.android.application") version "7.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.4.2" apply false
+    id("dagger.hilt.android.plugin") version "2.41" apply false
+    id("com.github.ben-manes.versions") version "0.42.0" apply false
+
+    // for release
 }
 
 tasks.create("clean", Delete::class) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "dagger.hilt.android.plugin" ->
+                    useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
