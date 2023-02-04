a272360be867e6219a3cf4bbcedf26f081b5636b
OHMAE Ryosuke <ryo@mm2d.net>
2023-02-05 00:26:51 +0900

    🆙Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "7.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
     id("dagger.hilt.android.plugin") version "2.44.2" apply false
     id("com.github.ben-manes.versions") version "0.44.0" apply false
