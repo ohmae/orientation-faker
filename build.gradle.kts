7d68dc301992ebe90274ad59d5b066886c0606a3
OHMAE Ryosuke <ryo@mm2d.net>
2023-02-04 14:43:17 +0900

    🆙AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "7.4.0" apply false
+    id("com.android.application") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
     id("dagger.hilt.android.plugin") version "2.44.2" apply false
